d361668a457b8ba149c83f868a66729482bd6e23
Tobias Haposan <tobias.haposan@student.pmsbe.ac.id>
2022-05-01 17:41:04 +0700

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
-#hotel index
+#hotel limit index
 maxFlowrateSPWH = float(500.0) #liters/30minutes
 maxFlowrateAB = float(3500.0) #liters/30minutes
 
