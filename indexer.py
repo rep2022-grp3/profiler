11c1a5f933a4004ce60e2d58715f75e6cf4d3888
Tobias Haposan <tobias.haposan@student.pmsbe.ac.id>
2022-05-08 20:24:44 +0700

    add profiling and occupancy input

--- indexer.py
+++ indexer.py
@@ -1,3 +1,4 @@
+from re import U
 import pandas as pd
 import numpy as np
 import os
@@ -9,3 +10,17 @@ maxFlowrateAB = float(3500.0) #liters/30minutes
 
 #load codex.csv
 codex = pd.read_csv(r'C:\Users\tobia\Videos\REP3\profiler\codex.csv')
+#assign variable codex-hour to first column, codexAmerican to second column, codexAsian to third column
+hour = codex['hour']
+codexAmerican = codex['american']
+codexAsian = codex['asian']
+
+#data input
+print("insert hotel occupancy rate")
+occupancyRate = float(input())
+print("hotel occupancy rate is", occupancyRate)
+
+#profile config
+print("all of the guests are Asian")
+#modify this later for racial profiling
+
