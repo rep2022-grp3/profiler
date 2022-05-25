25221f75054011dee4aaa47945312398f1a18e01
Tobias Haposan <tobias.haposan@student.pmsbe.ac.id>
2022-05-25 14:40:25 +0700

    add 2nd indexer (fix)

---
+++ indexer-fixed.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib as mpl
+
+#hotel limit index
+maxFlowrateSPWH = float(500.0) #liters/30minutes
+maxFlowrateAB = float(3500.0) #liters/30minutes
