33b8edfdaf218e01f2665f06918024ab696e8e85
Tobias Haposan <tobias.haposan@student.pmsbe.ac.id>
2022-06-23 01:52:39 +0700

    Update indexer-fixed.py

--- indexer-fixed.py
+++ indexer-fixed.py
@@ -3,3 +3,15 @@ import numpy as np
 import os
 import matplotlib as mpl
 
+#hotel limit index
+maxFlowrateSPWH = float(500.0) #liters/30minutes
+maxFlowrateAB = float(3500.0) #liters/30minutes
+
+#load codex.csv
+codex = pd.read_csv(r'C:\Users\tobia\Videos\REP3\profiler\codex.csv')
+#assign variable codex-hour to first column, codexAmerican to second column, codexAsian to third column
+hour = codex['hour']
+codexAmerican = codex['indv-rate-american']
+codexAsian = codex['indv-rate-asian']
+#print codex.csv
+print(codex)
